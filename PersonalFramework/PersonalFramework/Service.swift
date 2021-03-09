bbc1dfa9050cef5fc2be995c79471f86dab507df
Zaid Ahmed <zaid.ahmed@pf.com.pk>
2021-03-09 15:04:18 +0500

    PRoject created

---
+++ PersonalFramework/PersonalFramework/Service.swift
@@ -0,0 +1,17 @@
+//
+//  Service.swift
+//  PersonalFramework
+//
+//  Created by Zaid Ahmed IoS on 09/03/2021.
+//
+
+import Foundation
+
+public class Service {
+    
+    private init() {}
+    
+    public func doSomething() -> String {
+        return "SDK initialize "
+    }
+}
